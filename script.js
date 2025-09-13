8a664a7aa7867f6a0c80fd0a464963a43c7c4bf8
Elizaveta <lisabeth.umarova@yandex.com>
2025-09-13 13:47:14 +0300

    сделала

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let positionLat = 10;
 let positionLong = 12;
 let addressLat = 15;
 let addressLong = 20;
-const distance = Math.sqrt((addressLat - positionLat) ** 2 + (addressLongg - positionLong) ** 2);
+const distance = Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2);
 console.log(distance);
